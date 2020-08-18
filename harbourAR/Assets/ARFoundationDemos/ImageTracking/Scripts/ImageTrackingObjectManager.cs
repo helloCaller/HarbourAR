ae243ba9487ee0e08b23e348d28d002cea6679fe
Luke Garwood <luke_gar@hotmail.com>
2020-08-18 15:52:05 -0400

    adding multi image tracking AR

---
+++ harbourAR/Assets/ARFoundationDemos/ImageTracking/Scripts/ImageTrackingObjectManager.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageTrackingObjectManager : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Image manager on the AR Session Origin")]
+    ARTrackedImageManager m_ImageManager;
+
+    /// <summary>
+    /// Get the <c>ARTrackedImageManager</c>
+    /// </summary>
+    public ARTrackedImageManager ImageManager
+    {
+        get => m_ImageManager;
+        set => m_ImageManager = value;
+    }
+
+    [SerializeField]
+    [Tooltip("Reference Image Library")]
+    XRReferenceImageLibrary m_ImageLibrary;
+
+    /// <summary>
+    /// Get the <c>XRReferenceImageLibrary</c>
+    /// </summary>
+    public XRReferenceImageLibrary ImageLibrary
+    {
+        get => m_ImageLibrary;
+        set => m_ImageLibrary = value;
+    }
+
+
+
+
+
+
+	[SerializeField]
+	[Tooltip("Prefab for tracked 1 image")]
+	GameObject m_OnePrefab;
+
+	/// <summary>
+	/// Get the one prefab
+	/// </summary>
+	public GameObject onePrefab
+	{
+		get => m_OnePrefab;
+		set => m_OnePrefab = value;
+	}
+
+	GameObject m_SpawnedOnePrefab;
+
+	/// <summary>
+	/// get the spawned one prefab
+	/// </summary>
+	public GameObject spawnedOnePrefab
+	{
+		get => m_SpawnedOnePrefab;
+		set => m_SpawnedOnePrefab = value;
+	}
+
+	[SerializeField]
+	[Tooltip("Prefab for tracked 2 image")]
+	GameObject m_TwoPrefab;
+
+	/// <summary>
+	/// get the two prefab
+	/// </summary>
+	public GameObject twoPrefab
+	{
+		get => m_TwoPrefab;
+		set => m_TwoPrefab = value;
+	}
+
+	GameObject m_SpawnedTwoPrefab;
+
+	/// <summary>
+	/// get the spawned two prefab
+	/// </summary>
+	public GameObject spawnedTwoPrefab
+	{
+		get => m_SpawnedTwoPrefab;
+		set => m_SpawnedTwoPrefab = value;
+	}
+
+	[SerializeField]
+	[Tooltip("Prefab for tracked 1 image")]
+	GameObject m_ThreePrefab;
+
+	/// <summary>
+	/// Get the one prefab
+	/// </summary>
+	public GameObject ThreePrefab
+	{
+		get => m_ThreePrefab;
+		set => m_ThreePrefab = value;
+	}
+
+	GameObject m_SpawnedThreePrefab;
+
+	/// <summary>
+	/// get the spawned one prefab
+	/// </summary>
+	public GameObject spawnedThreePrefab
+	{
+		get => m_SpawnedThreePrefab;
+		set => m_SpawnedThreePrefab = value;
+	}
+
+	[SerializeField]
+	[Tooltip("Prefab for tracked 1 image")]
+	GameObject m_FourPrefab;
+
+	/// <summary>
+	/// Get the one prefab
+	/// </summary>
+	public GameObject FourPrefab
+	{
+		get => m_FourPrefab;
+		set => m_FourPrefab = value;
+	}
+
+	GameObject m_SpawnedFourPrefab;
+
+	/// <summary>
+	/// get the spawned one prefab
+	/// </summary>
+	public GameObject spawnedFourPrefab
+	{
+		get => m_SpawnedFourPrefab;
+		set => m_SpawnedFourPrefab = value;
+	}
+
+	int m_NumberOfTrackedImages;
+
+	private GameObject[] Prefabs;
+
+	NumberManager m_OneNumberManager;
+	NumberManager m_TwoNumberManager;
+	NumberManager m_ThreeNumberManager;
+	NumberManager m_FourNumberManager;
+
+	static Guid s_FirstImageGUID;
+	static Guid s_SecondImageGUID;
+	static Guid s_ThirdImageGUID;
+	static Guid s_FourthImageGUID;
+
+	//private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+
+
+	void OnEnable()
+    {
+		s_FirstImageGUID = m_ImageLibrary[0].guid;
+		s_SecondImageGUID = m_ImageLibrary[1].guid;
+		s_ThirdImageGUID = m_ImageLibrary[2].guid;
+		s_FourthImageGUID = m_ImageLibrary[3].guid;
+		Guid[] guids = { s_FirstImageGUID, s_SecondImageGUID, s_ThirdImageGUID, s_FourthImageGUID };
+		GameObject[] Prefabs = { m_OnePrefab, m_TwoPrefab, m_ThreePrefab, m_FourPrefab };
+
+
+
+
+
+		m_ImageManager.trackedImagesChanged += ImageManagerOnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_ImageManager.trackedImagesChanged -= ImageManagerOnTrackedImagesChanged;
+    }
+
+    void ImageManagerOnTrackedImagesChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        // added, spawn prefab
+        foreach(ARTrackedImage image in obj.added)
+        {
+
+			if (image.referenceImage.guid == s_FirstImageGUID)
+			{
+				m_SpawnedOnePrefab = Instantiate(m_OnePrefab, image.transform.position, image.transform.rotation);
+				m_OneNumberManager = m_SpawnedOnePrefab.GetComponent<NumberManager>();
+        
+        
+			}
+			else if (image.referenceImage.guid == s_SecondImageGUID)
+			{
+				m_SpawnedTwoPrefab = Instantiate(m_TwoPrefab, image.transform.position, image.transform.rotation);
+				m_TwoNumberManager = m_SpawnedTwoPrefab.GetComponent<NumberManager>();
+				
+
+			}
+			else if (image.referenceImage.guid == s_ThirdImageGUID)
+			{
+				m_SpawnedThreePrefab = Instantiate(m_ThreePrefab, image.transform.position, image.transform.rotation);
+				m_ThreeNumberManager = m_SpawnedThreePrefab.GetComponent<NumberManager>();
+			}
+			else if (image.referenceImage.guid == s_FourthImageGUID)
+			{
+				m_SpawnedFourPrefab = Instantiate(m_FourPrefab, image.transform.position, image.transform.rotation);
+				m_FourNumberManager = m_SpawnedFourPrefab.GetComponent<NumberManager>();
+			}
+
+		}
+
+	// updated, set prefab position and rotation
+	foreach (ARTrackedImage image in obj.updated)
+	{
+			Debug.Log(image.trackingState);
+			print(image.trackingState);
+		// image is tracking or tracking with limited state, show visuals and update it's position and rotation
+		if (image.trackingState == TrackingState.Tracking)
+		{
+
+				if (image.referenceImage.guid == s_FirstImageGUID)
+				{
+					m_OneNumberManager.Enable3DNumber(true);
+					m_SpawnedOnePrefab.transform.SetPositionAndRotation(image.transform.position, image.transform.rotation);
+
+				}
+				else if (image.referenceImage.guid == s_SecondImageGUID)
+				{
+					m_TwoNumberManager.Enable3DNumber(true);
+					m_SpawnedTwoPrefab.transform.SetPositionAndRotation(image.transform.position, image.transform.rotation);
+					
+				}
+				else if (image.referenceImage.guid == s_ThirdImageGUID)
+				{
+					m_ThreeNumberManager.Enable3DNumber(true);
+					m_SpawnedThreePrefab.transform.SetPositionAndRotation(image.transform.position, image.transform.rotation);
+				}
+				else if (image.referenceImage.guid == s_FourthImageGUID)
+				{
+					m_FourNumberManager.Enable3DNumber(true);
+					m_SpawnedFourPrefab.transform.SetPositionAndRotation(image.transform.position, image.transform.rotation);
+				}
+			}
+		// image is no longer tracking, disable visuals TrackingState.Limited TrackingState.None
+		else if (image.trackingState == TrackingState.Limited)
+		{
+				m_OneNumberManager.Enable3DNumber(false);
+				m_TwoNumberManager.Enable3DNumber(false);
+				m_ThreeNumberManager.Enable3DNumber(false);
+				m_FourNumberManager.Enable3DNumber(false);
+
+				//if (image.referenceImage.guid == s_FirstImageGUID)
+				//{
+				//	m_OneNumberManager.Enable3DNumber(false);
+				//	Destroy(m_SpawnedOnePrefab);
+				//}
+				//else if (image.referenceImage.guid == s_SecondImageGUID)
+				//{
+				//	m_TwoNumberManager.Enable3DNumber(false);
+				//	Destroy(m_SpawnedTwoPrefab);
+				//}
+				//else if (image.referenceImage.guid == s_ThirdImageGUID)
+				//{
+				//	m_ThreeNumberManager.Enable3DNumber(false);
+				//}
+				//else if (image.referenceImage.guid == s_FourthImageGUID)
+				//{
+				//	m_FourNumberManager.Enable3DNumber(false);
+				//}
+			}
+		}
+	
+        
+        // removed, destroy spawned instance
+        foreach(ARTrackedImage image in obj.removed)
+        {
+					if (image.referenceImage.guid == s_FirstImageGUID)
+            {
+                Destroy(m_SpawnedOnePrefab);
+              }
+            else if (image.referenceImage.guid == s_FirstImageGUID)
+            {
+                Destroy(m_SpawnedTwoPrefab);
+            }
+			else if (image.referenceImage.guid == s_ThirdImageGUID)
+			{
+				Destroy(m_SpawnedThreePrefab);
+			}
+			else if (image.referenceImage.guid == s_FourthImageGUID)
+			{
+				Destroy(m_SpawnedFourPrefab);
+			}
+		}
+
+ }
+
+    public int NumberOfTrackedImages()
+    {
+        m_NumberOfTrackedImages = 0;
+        foreach (ARTrackedImage image in m_ImageManager.trackables)
+        {
+            if (image.trackingState == TrackingState.Tracking)
+            {
+                m_NumberOfTrackedImages++;
+            }
+        }
+        return m_NumberOfTrackedImages;
+    }
+}
---
+++ harbourAR/Assets/DestroyParentTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParentTimer : MonoBehaviour
+{
+  public GameObject parentObj;
+  private float timeLeft = 15.0f;
+  // Start is called before the first frame update
+  void Start()
+  {
+    
+
+  }
+
+  private void Update()
+  {
+    timeLeft -= Time.deltaTime;
+   
+    if (timeLeft < 0)
+    {
+      Destroy(parentObj);
+    }
+  }
+
+}
