11d7435e012addd0bcc0842f44c7ebd1695be971
Luke Garwood <luke_gar@hotmail.com>
2020-09-05 23:22:30 -0400

    optimized project

---
+++ harbourAR/Assets/ResolutionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+
+  private int screenHeight;
+  private int screenWidth;
+  // Start is called before the first frame update
+  void Awake()
+  {
+    screenHeight = Screen.height / 2;
+    screenWidth = Screen.width / 2;
+
+    Screen.SetResolution(screenWidth, screenHeight, true);
+  }
+
+}
